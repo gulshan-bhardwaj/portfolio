82cddf3021ae57c1c8cf0ba9f21634de54da2d48
gulshan-bhardwaj <gulshanbhardwaj863@gmail.com>
2025-03-22 13:28:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const hamburger = document.getElementById('hamburger');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', function () {
+    const hamIcon = this.querySelector('.hamburger-icon');
+    const crossIcon = this.querySelector('.cross-icon');
+    if (hamIcon.style.display === "none") {
+        hamIcon.style.display = "inline-block"
+        menu.style.display = "none"
+        crossIcon.style.display = "none"
+    }
+    else {
+        crossIcon.style.display = "inline-block"
+        hamIcon.style.display = "none"
+        menu.style.display = "block"
+    }
+});
+const skillLevels = document.querySelectorAll('.skill-level');
+
+skillLevels.forEach((level) => {
+    level.addEventListener('mouseover', () => {
+        const progress = level.querySelector('.progress');
+        progress.style.width = level.querySelector('.level').textContent;
+    });
+});
